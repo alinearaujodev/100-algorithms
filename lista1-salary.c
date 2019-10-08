429fff760a2d6fc44cf101228997cdef8f08a1f8
Aline de Araújo <31042530+srtalinearaujo@users.noreply.github.com>
2019-10-08 11:51:20 -0300

    Add files via upload

---
+++ lista1-salary.c
@@ -0,0 +1,29 @@
+/*
+	Name: Salary
+	Copyright: 
+	Author: Aline
+	Date: 07/10/19 11:11
+	Description: leia num do funcionario, horas trabalhadas e valor da hora e calcule e imprima o salario e num.
+*/
+
+#include <stdio.h>
+
+int main(){
+	int num, horas_t;	
+	float salary=0, val_t;
+	
+	scanf("%d", &num);
+	getchar();
+	scanf("%d", &horas_t);
+	getchar();
+	scanf("%f", &val_t);
+	getchar();
+	
+	salary = horas_t * val_t;
+
+	
+	printf("NUMBER = %d\n", num);
+	printf("SALARY = U$ %.2f\n", salary);
+
+	return 0;
+}
